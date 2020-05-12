63c4bbce005ea067cf4065dbe6553823113e863e
Kate Sorotos <katesorotos@Kates-MacBook-Air.local>
2020-05-12 17:02:14 +0100

    hannah-blog: dynamic generation of pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout';
 
@@ -13,6 +13,9 @@ const BlogPage = () => {
                         title
                         date
                         }
+                    fields {
+                        slug
+                    }
                 }
             }
         }
@@ -28,8 +31,10 @@ const BlogPage = () => {
                     data.allMarkdownRemark.edges.map((edge) => {
                         return (
                             <li>
+                                <Link to={`/blog/${edge.node.fields.slug}`}>
                                 <h2>{edge.node.frontmatter.title}</h2>
                                 <p>{edge.node.frontmatter.date}</p>
+                                </Link>
                             </li>
                         )
                     })   
